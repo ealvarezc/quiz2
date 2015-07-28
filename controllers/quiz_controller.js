389c61f74a30871e43413db51789b873a4c7c090
ealvarezc <ealvarezc@users.noreply.github.com>
2015-07-28 22:19:51 +0100

    Editar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -48,6 +48,13 @@ exports.answer = function(req, res){
   );
 };
 
+// GET /quizes/:id/edit
+exports.edit = function(req, res){
+  var quiz = req.quiz; //autoload de instancia de quiz
+
+  res.render('quizes/edit', { quiz: quiz, errors: [] });
+};
+
 // GET /quizes/new
 exports.new = function(req, res){
   var quiz = models.Quiz.build( //crea objeto quiz
@@ -76,6 +83,26 @@ exports.create = function(req, res){
     }
   );
 }; 
+
+// PUT /quizes/:id
+exports.update = function(req, res){
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+
+  req.quiz
+  .validate()
+  .then(
+    function(err){
+      if(err){
+        res.render('quizes/edit', { quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en BD
+        .save({ fields: ["pregunta", "respuesta"] })
+        .then( function(){ res.redirect('/quizes'); } );
+      }
+    }
+  );
+};
 
 // GET /author
 exports.author = function(req, res){
